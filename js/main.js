99cd6a4d212ae2bdc959b106ddb77d7b53c6d259
Daniel <daniel.gayao7@gmail.com>
2021-09-26 16:34:04 +0800

    version 1

---
+++ js/main.js
@@ -0,0 +1,73 @@
+TweenMax.staggerFrom(
+    ".navbar > div",
+    0.8,
+    {
+        x: "-80",
+        opacity: 0,
+        ease: Power3.easeOut,
+        delay: 1,
+    },
+    0.08
+);
+
+TweenMax.staggerFrom(
+    ".images > div",
+    0.8,
+    {
+        x: "-80",
+        opacity: 0,
+        ease: Power3.easeOut,
+        delay: 1,
+    },
+    0.08
+);
+
+TweenMax.staggerTo(
+    ".navbar > div",
+    0.8,
+    {
+        x: "80",
+        opacity: 0,
+        ease: Power3.easeOut,
+        delay: 4,
+    },
+    0.04
+);
+
+TweenMax.staggerTo(
+    ".images > div",
+    0.4,
+    {
+        scale: "0",
+        opacity: 0,
+        ease: Power3.easeOut,
+        delay: 4,
+    },
+    0.04
+);
+
+TweenMax.to(
+    ".overlay",
+    1.2,
+    {
+        top: "-100%",
+        ease: Expo.easeOut,
+        delay: 5,
+    },
+    0.04
+);
+
+var textWrapper = document.querySelector(".header");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+    /\S/g,
+    "<span class='letter'>$&</span>"
+);
+
+anime.timeline().add({
+    targets: ".header .letter",
+    translateY: [100, 0],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 2000,
+    delay: (el, i) => 4800 + 40 * i,
+});
